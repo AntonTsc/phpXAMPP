1c94123aac37af9fd06ac8723bf843a296cd531c
2AW3 User 22 <2AW3-22@yourdomain.com>
2024-09-19 13:00:11 +0200

    terminar

--- Ej3 - Arrays/ejercicio11.php
+++ Ej3 - Arrays/ejercicio11.php
@@ -3,13 +3,23 @@
 
     Crea un array asociativo y ordenalo dependiendo del key y del valor. Para ello has de utilizar las funciones ksort y natsort. */
 
-    $semana = array("lunes" => 10,
-    "martes" => 20,
-    "miercoles" => 30,
-    "jueves" => 40,
-    "viernes" => 50,
-    "sabado" => 60,
-    "domingo" => 70);
+    $users = array("jordi" => "Jordi Rubio",
+    "marta" => "Marta Fabra",
+    "eva" => "Eva Palacio",
+    "toni" => "Antonio Comas",
+    "emilio" => "Emilio Morales",
+    "feo" => "Francisco Cascales");
 
-    echo natsort($semana)
+    //ksort($users); //Ordenar por clave (letras)
+    //natsort($users); //Ordenar por valor (numeros)
+
+    echo "<h1>Array asociativo</h1><h3>Mostrar el array</h3><br>";
+    foreach($users as $user => $valor){
+        echo "$user <br> $valor" . "";
+    }
+
+    /*foreach($abc as $letra => $num){
+        echo "$letra = $num <br>";
+    }*/
+        
 ?>
---
+++ Ej3 - Arrays/ejercicio12.php
@@ -0,0 +1,7 @@
+<?php
+    /* En el documento sale como que este ejercicio es el 0, pero lo pongo como el numero 12
+    
+    Programa que guarde 50 números aleatorios (rango del 0 al 49) en una tabla, luego tiene que indicar cuántas veces aparece cada número en el array.*/
+
+    
+?>
