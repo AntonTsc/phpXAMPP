5191335e4b82dcc86aa677ea9ccdcc286218ad25
2AW3 User 22 <2AW3-22@yourdomain.com>
2024-10-07 10:18:37 +0200

    error arreglado del ejemplo1 MVC

--- MVC/Ejemplo1/controller/personas_controlador.php
+++ MVC/Ejemplo1/controller/personas_controlador.php
@@ -1,7 +1,7 @@
 <?php
-    require_once("../model/personas_modelo.php");
+    require_once("model/personas_modelo.php");
     $per = new personas_modelo();
     $datos = $per->get_personas();
 
-    require_once("../views/personas_vista2.php");
+    require_once("views/personas_vista2.php");
 ?>
